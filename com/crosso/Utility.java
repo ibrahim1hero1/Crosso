1fe46bd8c13c0f3c9348e15e9d2d3b880c4d3f4e
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-19 23:57:13 +0200

    Create Utility.java

---
+++ com/crosso/Utility.java
@@ -0,0 +1,46 @@
+
+﻿/*
+ *   Crosso Scientific Calculator
+ *   hint this code under ApacheLicense
+ */
+
+
+
+﻿package com.crosso;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * 
+ * @author Ibrahim Abdsaid Hanna 
+ *         ibrahim.seniore@gmail.com
+ */
+
+
+public class Utility {
+
+	
+	
+	
+	public static String applyOperand(String formula,char c,int position){		
+		return formula.substring(0, position)+c+formula.substring(position, formula.length());    
+	}
+	
+	public static String applyOperand(String formula,String c,int position){		
+		return formula.substring(0, position)+c+formula.substring(position, formula.length());    
+	}
+	
+	
+	public static void reset(Button equal_btn,EditText formulaEditText,String formulaText){
+		
+		if(formulaText.trim().equals(Const.ERROR) ){
+			formulaEditText.setText("");
+		}
+		
+		equal_btn.setText("<<");
+	}
+	
+	
+	
+}
