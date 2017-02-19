b6ef30387078acbd244e9f23c17ff0b1d16ec140
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-19 23:52:51 +0200

    Create Const.java

---
+++ com/crosso/Const.java
@@ -0,0 +1,21 @@
+
+﻿/*
+ *   Crosso Scientific Calculator
+ *   hint this code under ApacheLicense
+ */
+
+
+
+﻿package com.crosso;
+
+/**
+ * 
+ * @author Ibrahim Abdsaid Hanna 
+ *         ibrahim.seniore@gmail.com
+ */
+
+
+public class Const {
+    static String ERROR="Error";
+
+}
